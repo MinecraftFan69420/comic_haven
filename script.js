d9ca98c6c328b7c84b93667858ec9b8d2fee4494
MinecraftFan69420 <gelay232000@yahoo.com>
2025-04-29 16:56:52 +0300

    Second try

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
         filter.addEventListener('change', () => {
             const activefilters = Array.from(filters).filter(filter => filter.checked).map(filter => filter.id)
             books.forEach(book => {
-                const bookcategories = book.getAttribute("filtercategory").split(" ")
+                const bookcategories = book.getAttribute("filtercategory").split(" ").filter(Boolean) || ""
                 if (activefilters.length === 0 || activefilters.some(filter => bookcategories.includes(filter))) {
                     book.style.display = ''
                 } else book.style.display = 'none'
